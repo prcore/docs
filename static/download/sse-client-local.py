0a2fd4d66c8f1524d56d7948dfa794b4564f63fe
Zhaosi Qu <git@chaos.run>
2023-03-07 13:47:25 +0200

    add local scripts

---
+++ static/download/sse-client-local.py
@@ -0,0 +1,56 @@
+import json
+import pprint
+import requests
+import sseclient
+
+BASE_URL = "http://localhost:8000" 
+PROJECT_ID = 8
+URL = f"{BASE_URL}/project/{PROJECT_ID}/stream/result"
+HEADERS = {
+  "Authorization": "Bearer UaJW0QvkMA1cVnOXB89E0NbLf3JRRoHwv2wWmaY5v=QYpaxr1UD9/FupeZ85sa2r"
+}
+
+
+def with_requests(url, headers):
+    # Get a streaming response for the given event feed using requests.
+    return requests.get(url, stream=True, headers=headers)
+
+
+response = with_requests(URL, HEADERS)
+client = sseclient.SSEClient(response)
+
+print("Waiting for events...")
+
+"""
+Please note that the word `event` here is the `E` from `SSE`, 
+not the `event` in the business processing perspective.
+The data of our `event` is in the `event.data` list.
+"""
+
+try:
+  for event in client.events():
+      if event.event != "message":
+          continue
+
+      event_data = json.loads(event.data)
+      first_event = event_data[0]
+      prescriptions = first_event["prescriptions"]
+      prescriptions_with_output = [prescriptions[p] for p in prescriptions if prescriptions[p]["output"]]
+
+      if not prescriptions_with_output:
+        continue
+
+      print(f"Received message: {event.event}")
+      print(f"ID: {event.id}")
+
+
+      print(f"Data type: {type(event_data)}")
+      print(f"Length: {len(event_data)}")
+
+      pprint.pprint(prescriptions_with_output, width=120)
+
+      print("-" * 24)
+except KeyboardInterrupt:
+  print("Interrupted by user")
+
+print("Done!")
---
+++ static/download/workflow-example-local-reading.py
@@ -0,0 +1,46 @@
+import requests
+from requests import Response
+
+# Change this to your own event log file
+BASE_URL = "https://prcore.chaos.run" 
+API_TOKEN = "UaJW0QvkMA1cVnOXB89E0NbLf3JRRoHwv2wWmaY5v=QYpaxr1UD9/FupeZ85sa2r"
+HEADERS = {
+    "Authorization": f"Bearer {API_TOKEN}"
+}
+REQUEST_HEADERS = {
+    "Authorization": f"Bearer {API_TOKEN}",
+    "Content-Type": "application/json"
+}
+PROJECT_ID = 26
+
+
+def get_project(project_id) -> Response:
+    # Get the project definition
+    url = f"{BASE_URL}/project/{project_id}"
+    response = requests.get(url, headers=HEADERS)
+    return response
+
+
+def main():
+    print("\nStaring the client...\n")
+
+    try:
+        # Get the project status
+        print("Getting the project status...")
+        i = 0
+        while True:
+            response = get_project(PROJECT_ID)
+            if response.status_code != 200:
+                print(f"[{i:08d}] = Error: {response.text}")
+            else:
+                project_status = response.json()["project"]["status"]
+                print(f"[{i:08d}] = Project status: {project_status}")
+            i += 1
+    except KeyboardInterrupt:
+        print("Interrupted by user\n")
+
+    print("Done!\n")
+
+
+if __name__ == "__main__":
+    main()
