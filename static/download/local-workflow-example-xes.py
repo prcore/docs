2145f80adff240bba88e76351d7fc4260ae3b4d2
Zhaosi Qu <git@chaos.run>
2023-03-18 18:48:37 +0200

    Site updated L 18 märts 2023 18:48:37 EET ...

--- static/download/local-workflow-example-xes.py
+++ static/download/local-workflow-example-xes.py
@@ -5,7 +5,7 @@ from time import sleep
 # Change this to your own event log file
 EVENT_LOG_FILE = "/home/zhaosi/Sites/PrCore/static/download/bpic2012-XES.zip"
 
-BASE_URL = "https://prcore.chaos.run"
+BASE_URL = "http://localhost:8000"
 API_TOKEN = "UaJW0QvkMA1cVnOXB89E0NbLf3JRRoHwv2wWmaY5v=QYpaxr1UD9/FupeZ85sa2r"
 HEADERS = {
     "Authorization": f"Bearer {API_TOKEN}"
